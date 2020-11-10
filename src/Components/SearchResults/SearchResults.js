b09c3b2a0db1ce834e07ca7485d21b71f4346bfb
qundax <qunda.lim@outlook.sg>
2020-11-10 20:51:46 +0800

    Added logic to render search results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './SearchResults.css';
+
+import Tracklist from '../Tracklist/Tracklist';
 
 export default class SearchResults extends React.Component {
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <!-- Add a TrackList component -->
+        <Tracklist searchResults={this.props.searchResults} />
       </div>
     )
   }
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Tracklist.css'
+
+import Track from '../Track/Track';
 
 export default class Tracklist extends React.Component {
   render() {
     return (
       <div className="TrackList">
-          <!-- You will add a map method that renders a set of Track components  -->
+          { this.props.searchResults.map((track) => {
+            <Track track={track} />
+          }) }
       </div>
     )
   }
